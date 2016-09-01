7e58a7f871f5fc082733a748c9ada33fe8b87274
William Kim <jesung1221@hotmail.com>
2016-09-01 14:06:48 +0800

    Add files via upload

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,1 +1,17 @@
+import {Mongo} from 'meteor/mongo';
+import validUrl from 'valid-url';
+import {check, Match} from 'meteor/check';
 
+Meteor.methods({
+	'links.insert':function(url){
+		check(url, Match.Where(url => validUrl.isUri(url))); //check throws an error
+	
+	//We're ready to save the url
+	const token = Math.random().toString().slice(-5); //generate random string
+	//Links.insert({url:url, token:token, clicks:0});
+	Links.insert({url, token, clicks:0});
+ }
+});
+
+
+export const Links = new Mongo.Collection('links');
